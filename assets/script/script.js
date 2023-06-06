3c8693009c6c5bdbd6aa3c7712f1fe3e8254e0a2
albina990 <albina.khadieva90@gmail.com>
2023-06-06 20:44:16 +0200

    fix2 script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,55 +1,57 @@
 /*
 Запрос должен уходить на сервер при нажатии на кнопку (нужно добавить обработчик). В разметке HTML должно быть минимум два поля: в первое нужно выводить результат поиска, если данные пришли и всё хорошо, во втором — ошибку, если что-то пошло не так. (Добейтесь, чтобы ваше приложение выводило понятные пользователю сообщения в случае ошибки, например «Сервер не доступен»). Сообщения должны быть видны поочередно, если результат показан, ошибка должна быть сброшена. И наоборот. Обязательно добавьте обработчик ответа: если ответ успешный, следующий обработчик then получит объект ответа на вход, если с ответом что-то не так, отклоните промис (для этого верните Promise.reject с кодом статуса ответа). Блок catch и finally использовать обязательно.
 */
-const select = document.querySelector(".select") //выпадающий список
-const button = document.querySelector("button") // кнопка "найти"
-const selectedTitle = select.querySelector(".select__title") //заголовок выпадающего списка
-const selectLabels = select.querySelectorAll(".select__label") // опции выпадающего списка
-const inputIndex = document.getElementById("index") // инпут для ввода номера
-const inputsRessource = document.querySelectorAll(
-    'input[name = "singleSelect"]'
-) //инпуты опций выпадающнго списка
-const preloader = document.querySelector(".preloader") //прелоадер
-const resolveOutput = document.querySelector(".output__resolve") //див, в который выводится результат при успешном выполнении
-const rejectOutput = document.querySelector(".output__reject") //див, в который выводится ошибка
-const finallyOutput = document.querySelector(".output__more") //див, в который выводится блок finally
+const select = document.querySelector(".select"); //выпадающий список
+const button = document.querySelector("button"); // кнопка "найти"
+const selectedTitle = select.querySelector(".select__title"); //заголовок выпадающего списка
+const selectLabels = select.querySelectorAll(".select__label"); // опции выпадающего списка
+const inputIndex = document.getElementById("index"); // инпут для ввода номера
+const inputsRessource = document.querySelectorAll('input[name = "singleSelect"]'); //инпуты опций выпадающнго списка
+const preloader = document.querySelector(".preloader"); //кружок загрузки
+const resolveOutput = document.querySelector(".output__resolve"); //див, в который выводится результат при успешном выполнении
+const rejectOutput = document.querySelector(".output__reject"); //див, в который выводится ошибка
+const finallyOutput = document.querySelector(".output__more"); //див, в который выводится блок finally
 
 // Функционал выпадающего списка
 // Разворачиват список при клике, если он неактивен и сворачивает, если был активен
 selectedTitle.addEventListener("click", () => {
     if ("active" === select.getAttribute("active")) {
-        select.removeAttribute("active")
+        select.removeAttribute("active");
     } else {
-        select.setAttribute("active", "active")
+        select.setAttribute("active", "active");
     }
 })
 // активирует опцию и сворачивает список
 for (let i = 0; i < selectLabels.length; i++) {
     selectLabels[i].addEventListener("click", (evt) => {
-        selectedTitle.textContent = evt.target.textContent
-        select.removeAttribute("active")
+        selectedTitle.textContent = evt.target.textContent;
+        select.removeAttribute("active");
     })
 }
 
-function getInfo(evt) {
-    // функция, которая выполняет запрос к api и выводит результат на странице
-    evt.preventDefault()
-    preloader.hidden = false // появление кружка загрузки
+function getInfo(evt) { // функция, которая выполняет запрос к api и выводит результат на странице
+    evt.preventDefault();
+    preloader.hidden = false; // появление кружка загрузки
     for (const ressource of inputsRessource) {
-        if (ressource.checked == true) {
-            // сбор значений выбранных инпутов
-            const valueRessource = ressource.value
-            const valueIndex = Number(inputIndex.value)
-            resolveOutput.hidden = true
-            rejectOutput.hidden = true
-            finallyOutput.hidden = true
+        if (ressource.checked == true) { // сбор значений выбранных инпутов
+            const valueRessource = ressource.value;
+            const valueIndex = Number(inputIndex.value);
+            resolveOutput.hidden = true;
+            rejectOutput.hidden = true;
+            finallyOutput.hidden = true;
+            let res; // переменная для ответа от сервера
             try {
-                if (valueRessource == "0") throw new Error("Invalid ressource") //выброс ошибки при пустом поле ressource
+                if (valueRessource == "0") throw new Error("Invalid ressource"); //выброс ошибки при пустом поле ressource
                 fetch(`https://swapi.dev/api/${valueRessource}/${valueIndex}/`)
-                    .then((response) => response.json())
+                    .then((response) => {
+                        res = response;
+                        return response.json();
+                    })
                     .then((json) => {
-                        if (json.detail == "Not found") throw new Error("Invalid number") //выброс ошибки при вводе некорректного индекса
-                        resolveOutput.hidden = false
+                        if (json.detail == "Not found") { //выброс ошибки при вводе некорректного индекса
+                            return Promise.reject(new Error("Error " + res.status + ". Invalid number"));
+                        }
+                        resolveOutput.hidden = false; // активирует окно для отображения информации
                         switch(valueRessource){
                             case "people": 
                                 resolveOutput.innerHTML = `<p>Name: ${json.name}</p>
@@ -77,23 +79,23 @@ function getInfo(evt) {
                                 <p>Vehicle class: ${json.vehicle_class}</p>`;
                                 break;
                         }
-                        preloader.hidden = true // прячет кружок загрузки
+                        preloader.hidden = true; // прячет кружок загрузки
                     })
                     .catch((err) => { //ловит ошибку некорректного индекса
-                        rejectOutput.hidden = false
-                        rejectOutput.innerHTML = `${err.message}. Please try again`
-                        preloader.hidden = true
+                        rejectOutput.hidden = false; // активирует окно ошибок
+                        rejectOutput.innerHTML = `${err.message}. Please try again.`;
+                        preloader.hidden = true;
                     })
             } catch (err) { //ловит ошибку некорректного ресурса
-                rejectOutput.hidden = false
-                rejectOutput.innerHTML = `${err.message}. Please try again`
-                preloader.hidden = true
+                rejectOutput.hidden = false;
+                rejectOutput.innerHTML = `${err.message}. Please try again.`;
+                preloader.hidden = true;
             } finally { // выполняется в любом случае
-                finallyOutput.hidden = false
-                finallyOutput.innerHTML = `<a href="https://en.wikipedia.org/wiki/Star_Wars">Learn more</a>`
+                finallyOutput.hidden = false;
+                finallyOutput.innerHTML = `<a href="https://en.wikipedia.org/wiki/Star_Wars">Learn more</a>`;
             }
         }
     }
 }
 
-button.addEventListener("click", getInfo) // событие на кнопке "search"
+button.addEventListener("click", getInfo); // событие на кнопке "search"
